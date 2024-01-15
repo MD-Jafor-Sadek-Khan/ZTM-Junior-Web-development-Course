df60a2bcd0cfdd0e15c8b6453fd5ac7db60d6c00
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-01-15 22:51:30 +0600

    Added Delete and Done option

--- DOM/144 - domevents/script.js
+++ DOM/144 - domevents/script.js
@@ -1,30 +1,59 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var button = document.getElementById("enter")
+var input = document.getElementById("userinput")
+var ul = document.querySelector("ul")
 
 function inputLength() {
-	return input.value.length;
+  return input.value.length
 }
 
+function createButton(string, type) {
+  var button = document.createElement("button")
+
+  button.classList.add(type)
+  button.appendChild(document.createTextNode(string))
+
+  return button
+}
+
+function deleteButton(event) {
+  var buttonDelete = event.target
+  var parent = buttonDelete.parentElement.parentElement
+  parent.removeChild(buttonDelete.parentElement)
+}
+
+function doneButton(event) {
+  var buttonDone = event.target
+  var parent = buttonDone.parentElement
+  parent.classList.add("done")
+}
 function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+  var li = document.createElement("li")
+  var buttonDone = createButton("Done", "btnone")
+  var buttonDelete = createButton("Delete", "delete")
+
+  li.appendChild(document.createTextNode(input.value))
+  li.appendChild(buttonDone)
+  li.appendChild(buttonDelete)
+  ul.appendChild(li)
+
+  input.value = ""
+
+  buttonDelete.addEventListener("click", deleteButton)
+  buttonDone.addEventListener("click", doneButton)
 }
 
 function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+  if (inputLength() > 0) {
+    createListElement()
+  }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement()
+  }
 }
 
-button.addEventListener("click", addListAfterClick);
+button.addEventListener("click", addListAfterClick)
 
-input.addEventListener("keypress", addListAfterKeypress);
+input.addEventListener("keypress", addListAfterKeypress)
