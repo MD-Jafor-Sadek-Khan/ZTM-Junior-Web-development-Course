b9c4ef73b6793b68d678cc5fc26cf0d4ca109031
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-01-15 23:37:50 +0600

    DOM Color Gradient Generator Project Done!

--- DOM/147 - backgroundgenerator/script.js
+++ DOM/147 - backgroundgenerator/script.js
@@ -1,19 +1,14 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+var color1 = document.querySelector(".color1")
+var color2 = document.querySelector(".color2")
 
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+function changeBackgroundGradient() {
+  var body = document.querySelector("#gradient")
+  var colorOutput = document.getElementsByTagName("h3")[0]
 
-	css.textContent = body.style.background + ";";
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value} )`
+
+  colorOutput.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
-
-color2.addEventListener("input", setGradient);
+color1.addEventListener("input", changeBackgroundGradient)
+color2.addEventListener("input", changeBackgroundGradient)
