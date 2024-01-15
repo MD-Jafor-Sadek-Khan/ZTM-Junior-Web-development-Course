23775bbbc88214f3aec7daebec12b6376b781ba9
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-01-16 03:02:39 +0600

    Done

--- Advanced JavaScript/155 - advanced-exercise-3/exercise3.js
+++ Advanced JavaScript/155 - advanced-exercise-3/exercise3.js
@@ -1,56 +1,66 @@
 // change everything below to the newer Javascript!
 
 // let + const
-var a = 'test';
-var b = true;
-var c = 789;
-a = 'test2';
+let a = "test"
+let b = true
+let c = 789
+a = "test2"
 
+console.log("let, const => ", a,b,c);
 
 // Destructuring
-var person = {
-    firstName : "John",
-    lastName  : "Doe",
-    age       : 50,
-    eyeColor  : "blue"
-};
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+}
 
-var firstName = person.firstName;
-var lastName = person.lastName;
-var age = person.age;
-var eyeColor = person.eyeColor;
+let {firstName, lastName, age, eyeColor} = person
 
+console.log("Destructuring",firstName,lastName,age,eyeColor);
 
 // Object properties
-var a = 'test';
-var b = true;
-var c = 789;
+ a = "test"
+ b = true
+ c = 789
 
-var okObj = {
-  a: a,
-  b: b,
-  c: c
-};
+const okObj = {
+  a,
+  b,
+  c
+}
+
+console.log("Object properties", okObj.a, okObj.b, okObj.c);
 
 
 // Template strings
-var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
+const city = "Liver Pool"
+
+let message = `Hello ${firstName} have I met you before? I think we met in ${city} last summer no???`
+
+console.log("Template strings",message);
 
 
 // default arguments
 // default age to 10;
-function isValidAge(age) {
-    return age
+function isValidAge(age = 10) {
+  return age
 }
+
+console.log("default arguments => ",isValidAge());
 
 // Symbol
 // Create a symbol: "This is my first Symbol"
 
+let sym = Symbol("This is my first Symbol")
+
+console.log("Symbol", sym);
 // Arrow functions
-function whereAmI(username, location) {
-    if (username && location) {
-        return "I am not lost";
-    } else {
-        return "I am totally lost!";
-    }
+
+const whereAmI = (username, location) =>{
+    return username && location ? "I am not lost" : "I am totally lost!"
 }
+
+
+console.log("Arrow functions", whereAmI(firstName, location));
