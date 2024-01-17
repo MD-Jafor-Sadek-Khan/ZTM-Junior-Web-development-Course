48c1b47621de9c5f0c76d16b0226fbbf9b9f57df
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-01-18 02:51:10 +0600

    Done

--- Advanced JavaScript/168 - advanced-exercise-es8/exerciseES8.js
+++ Advanced JavaScript/168 - advanced-exercise-es8/exerciseES8.js
@@ -10,6 +10,9 @@ let rabbit = '🐇';
 '       🐢'
 '       🐇'
 
+turtle.padStart(8)
+rabbit.padStart(8)
+
 // when you do:
 console.log(startLine);
 console.log(turtle);
@@ -20,6 +23,15 @@ console.log(rabbit);
 // Read about what the second parameter does in padEnd and padStart
 turtle = turtle.trim().padEnd(9, '=');
 
+console.log(turtle);
+
+
+
+
+
+
+
+
 
 // #3) Get the below object to go from:
 let obj = {
@@ -29,3 +41,6 @@ let obj = {
 }
 // to this:
 'my name is Rudolf the raindeer'
+
+const sentence = Object.entries(obj).flat().join(' ')
+console.log(sentence);
