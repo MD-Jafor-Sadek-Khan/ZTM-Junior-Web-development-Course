e5b9b5078266e4e4af844e552da5a08d0ced0989
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-01-18 03:14:47 +0600

    done

---
+++ Advanced JavaScript/171 - advanced-exercise-es10/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+
+
+//#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
+const users = { user1: 18273, user2: 92833, user3: 90315 }
+console.log(Object.entries(users));
+
+
+
+//#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
+
+console.log(Object.entries(users).map(arr =>[arr[0] ,arr[1]*2]));
+
+//#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
+
+console.log(Object.fromEntries(Object.entries(users).map(arr =>[arr[0] ,arr[1]*2])));
