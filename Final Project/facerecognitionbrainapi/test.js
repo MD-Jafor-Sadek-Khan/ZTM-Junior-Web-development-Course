69dd97ddedb793ec736bbc7fe3e41054c79d6e44
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-01-26 14:40:16 +0600

    bcrypt version test successful

---
+++ Final Project/facerecognitionbrainapi/test.js
@@ -0,0 +1,57 @@
+const bcrypt = require("bcrypt-nodejs")
+
+// var hash = bcrypt.hashSync("bacon");
+
+// bcrypt.compareSync("bacon", hash); // true
+// bcrypt.compareSync("veggies", hash); // false
+
+// bcrypt.hash("bacon", null, null, function(err, hash) {
+//     //! Store hash in your password DB.
+// });
+
+// //! Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     //! res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     //! res = false
+// });
+
+const dataBase = {
+  userList: [],
+}
+
+const { userList} = dataBase
+
+// Synchronous
+
+var synHash = bcrypt.hashSync("bacon")
+// console.log(hash)
+
+// const synState = bcrypt.compareSync("bacon", hash) // true
+// console.log(state)
+
+// const synState2 = bcrypt.compareSync("veggies", hash) // false
+// console.log(state2)
+
+// Asynchornous
+
+bcrypt.hash("bacon", null, null, function (err, hash) {
+  dataBase.userList.push({
+    name:"rahat",
+    password:hash
+  })
+})
+bcrypt.hash("bacon", null, null, function (err, hash) {
+  dataBase.userList.push({
+    name:"rahat",
+    password:hash
+  })
+})
+
+
+
+setTimeout(() => {
+    const synState = bcrypt.compareSync("bacon", userList[0].password)
+    console.log(synState)
+}, 0);
